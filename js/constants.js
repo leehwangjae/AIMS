994cde81236309927340f9f496ed37593b1b95c5
leehwangjae <ysmusic98@gmail.com>
2026-06-05 15:30:17 +0900

    feat: expand permissions for unit task menus

--- js/constants.js
+++ js/constants.js
@@ -5,13 +5,26 @@ export const ROLES = {
   VIEWER: 'VIEWER'
 };
 
+const ALL_ROLES = [ROLES.MASTER, ROLES.PROFESSOR, ROLES.STAFF, ROLES.VIEWER];
+const EDIT_ROLES = [ROLES.MASTER, ROLES.PROFESSOR, ROLES.STAFF];
+
 export const MENU_PERMISSIONS = {
-  dashboard: [ROLES.MASTER, ROLES.PROFESSOR, ROLES.STAFF, ROLES.VIEWER],
-  projects: [ROLES.MASTER, ROLES.PROFESSOR, ROLES.STAFF],
-  performance: [ROLES.MASTER, ROLES.PROFESSOR, ROLES.STAFF],
+  default: ALL_ROLES,
+  dashboard: ALL_ROLES,
+
+  'business-1-1': EDIT_ROLES,
+  'business-1-2': EDIT_ROLES,
+  'business-1-3': EDIT_ROLES,
+  'business-2-1-ai': EDIT_ROLES,
+
+  'kpi-1-1': ALL_ROLES,
+  'kpi-1-2': ALL_ROLES,
+  'kpi-1-3': ALL_ROLES,
+  'kpi-2-1-ai': ALL_ROLES,
+
   budgets: [ROLES.MASTER, ROLES.PROFESSOR],
-  reports: [ROLES.MASTER, ROLES.PROFESSOR, ROLES.STAFF],
-  users: [ROLES.MASTER],
+  reports: EDIT_ROLES,
+  'ai-center': EDIT_ROLES,
   settings: [ROLES.MASTER]
 };
 
